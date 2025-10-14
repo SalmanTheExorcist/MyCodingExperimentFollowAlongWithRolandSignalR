50203cc59c4e9154c69b10089c96f7a965c2508c
Demonically Possessed Exorcist Salman <peaceful.salman@yahoo.com>
2025-10-15 02:16:29 +0300

    Initial commit after initial wire up

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MyFreshlyBakedMVCWebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿(function(){
+    'use strict'
+  
+   
+    const timestamp = Date.now();
+
+    // Convert timestamp to a readable date and time format
+    const currentDate = new Date(timestamp);
+
+    // Formatting date 
+    const formattedDate = currentDate.toLocaleString();
+
+
+    console.log(`Inside site.js: ${formattedDate}`);
+
+ 
+
+})();
