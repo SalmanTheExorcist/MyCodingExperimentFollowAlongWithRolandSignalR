8e555bd594add37a9b72c361edfd76ae7fe82b7f
Demonically Possessed Exorcist Salman <peaceful.salman@yahoo.com>
2025-10-15 02:50:01 +0300

    Added Video element stream webcam

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,7 +1,7 @@
-﻿(function(){
+﻿(function () {
     'use strict'
-  
-   
+
+
     const timestamp = Date.now();
 
     // Convert timestamp to a readable date and time format
@@ -13,6 +13,19 @@
 
     console.log(`Inside site.js: ${formattedDate}`);
 
- 
+
 
 })();
+
+async function btnStarTheAction() {
+    console.log("Inside: btnStarTheAction()");
+
+    try 
+    {
+      const mediaStream = await navigator.mediaDevices.getUserMedia({ video: true });
+      const videoMyWebCam = document.getElementById('videoMyWebCam');
+      videoMyWebCam.srcObject = mediaStream;
+    } catch (error) {
+      console.error('Error accessing webcam: ', error);
+    }
+};       
