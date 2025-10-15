7fe75964e5bb6ee85bcf51c68cff979d37c9761d
Demonically Possessed Exorcist Salman <peaceful.salman@yahoo.com>
2025-10-15 22:31:46 +0300

    Updated our site.js added StopVideo button

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
+using MyFreshlyBakedMVCWebApp.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddSignalR(o => o.EnableDetailedErrors = true);
+
 
 var app = builder.Build();
 
@@ -23,5 +28,9 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+app.MapHub<MySuperFancyHub>("/mysuperfancyhub");
+
 
 app.Run();
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,8 +1,88 @@
 ﻿(function () {
-    'use strict'
+  'use strict'
 
+  logToConsole("Inside site.js");
 
-    const timestamp = Date.now();
+})();
+
+/* ------------------------------------------------------- */
+const btnStarTheAction = async function () {
+  logToConsole("Inside: btnStarTheAction()");
+
+  
+  try {
+    const mediaStream = await navigator.mediaDevices.getUserMedia(
+        { 
+          video: true,
+          audio: false 
+        }
+      
+      );
+    const videoMyWebCam = document.getElementById('videoMyWebCam');
+    videoMyWebCam.srcObject = mediaStream;
+  } catch (error) {
+    console.error('Error accessing webcam: ', error);
+  }
+};
+/* ------------------------------------------------------- */
+
+//--A function/method to initialize our SIgnalR Connection
+//---The SignalR connection is always initiated by the client.
+let myConnection = {};
+
+const myInitializeSignalRConnection = function () {
+  logToConsole("Inside: myInitializeSignalRConnection()");
+
+  const mySignalRConnection = new signalR.HubConnectionBuilder()
+    .withUrl("/mysuperfancyhub")
+    .build();
+  mySignalRConnection.start()
+    .catch(someError => console.log(someError.toString()));
+
+  //return mySignalRConnection;                     
+  myConnection = mySignalRConnection;
+
+};
+/* ------------------------------------------------------- */
+
+const myInitializeIndexPageStuff = function () {
+  logToConsole("Inside: myInitializeIndexPageStuff()");
+
+  let videoMyWebCam = document.getElementById('videoMyWebCam');
+  let txtVideoTimeDisplay = document.getElementById("txtVideoTimeDisplay");
+  let btnPlayVideo = document.getElementById("btnPlayVideo");
+  let btnStopVideo = document.getElementById("btnStopVideo");
+
+  videoMyWebCam.addEventListener("ended",function(){
+        videoMyWebCam.currentTime = 0;
+        logToConsole("Video Ended.");
+    });
+
+  videoMyWebCam.addEventListener("timeupdate", function () {
+     setVideoTimeDisplay(Math.floor(videoMyWebCam.currentTime));
+    //console.log("VideoTimeUpdate.");
+  });
+
+  btnPlayVideo.addEventListener("click",function(){
+        //videoMyWebCam.play();
+        btnStarTheAction();
+        logToConsole("Playing Video");
+  });
+
+  btnStopVideo.addEventListener("click",function(){
+      videoMyWebCam.pause();
+      videoMyWebCam.srcObject = null;
+      //videoMyWebCam.currentTime = 0;
+      setVideoTimeDisplay(0);
+
+      logToConsole("Stopped Video");
+    });
+
+}
+/* ------------------------------------------------------- */
+  function logToConsole(strLogMessage){
+
+     const timestamp = Date.now();
 
     // Convert timestamp to a readable date and time format
     const currentDate = new Date(timestamp);
@@ -10,22 +90,13 @@
     // Formatting date 
     const formattedDate = currentDate.toLocaleString();
 
+     console.log(`${formattedDate}: ${strLogMessage}`);
 
-    console.log(`Inside site.js: ${formattedDate}`);
+     
+  }
+/* ------------------------------------------------------- */
+function setVideoTimeDisplay(strValue){
+  txtVideoTimeDisplay.innerText = strValue;
+}
+/* ------------------------------------------------------- */
 
-
-
-})();
-
-async function btnStarTheAction() {
-    console.log("Inside: btnStarTheAction()");
-
-    try 
-    {
-      const mediaStream = await navigator.mediaDevices.getUserMedia({ video: true });
-      const videoMyWebCam = document.getElementById('videoMyWebCam');
-      videoMyWebCam.srcObject = mediaStream;
-    } catch (error) {
-      console.error('Error accessing webcam: ', error);
-    }
-};       
