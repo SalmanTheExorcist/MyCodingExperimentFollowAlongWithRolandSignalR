5d34054333ad2cedeec90316e77221d995ac124e
Demonically Possessed Exorcist Salman <peaceful.salman@yahoo.com>
2025-10-16 19:47:10 +0300

    Added ImageRelatedStuff ControllerActionView

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,6 +19,11 @@ public class HomeController : Controller
     }
 
   
+    public IActionResult ImagesRelatedStuff()
+    {
+        return View();
+    }
+
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
