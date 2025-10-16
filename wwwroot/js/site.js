4332818dc55cd8eac1704e8ab05248f631956b4b
Demonically Possessed Exorcist Salman <peaceful.salman@yahoo.com>
2025-10-17 01:08:25 +0300

    Converting FileInput Image to Base64URL into img-src

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -56,16 +56,19 @@ const myInitializeSignalRConnection = function () {
 
 };
 /* ------------------------------------------------------- */
+
+var strImageBase64 = "";
 const myInitializeImageRelatedStuff = function(){
    logToConsole("Inside: myInitializeImageRelatedStuff()");
    const allowedExtensions =  ['jpg', 'jpeg', 'png', 'gif'];
-
+ 
+  
    let fileSomeImage = document.getElementById("fileSomeImage");
 
   fileSomeImage.addEventListener("change",function(event){
 
       
-      var singleFile = fileSomeImage.files[0];
+      var singleFile = event.target.files[0];
       var singleFileName = singleFile.name.toLowerCase();
       var singleFileExtension = singleFileName
                       .substring(singleFileName.lastIndexOf('.') + 1);
@@ -87,14 +90,32 @@ const myInitializeImageRelatedStuff = function(){
 
       setResultsInnerText(txtOutputMessage);
 
+       let imageFromInputFile = document.getElementById("imageFromInputFile");
+       imageFromInputFile.src = "";
+       strImageBase64 = "";
+       
       if(boolFileIsGoodToGo){
         //Do more stuff with the selected File/Picture
+          var myFileReader = new FileReader();
+        
 
-      }
+          myFileReader.onload = function(e){
+            strImageBase64 = e.target.result;           
+            imageFromInputFile.src = strImageBase64;                                                                  
+          };
+          myFileReader.readAsDataURL(singleFile);     
+
+      };
+    
   });
 
-}
+ 
 
+}
+/* ----------------------------------------------------- */
+function btnPrintBase64_click(){
+  logToConsole(strImageBase64);
+}
 
 
 /* ------------------------------------------------------ */
