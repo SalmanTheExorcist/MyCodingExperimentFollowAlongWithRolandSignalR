42c24621ac05239e2585fe998650aae0deb1f495
Demonically Possessed Exorcist Salman <peaceful.salman@yahoo.com>
2025-10-18 01:40:58 +0300

    Loaded InMemoryList into MyFancyPlayground View

--- wwwroot/js/home-myfancyplayground.js
+++ wwwroot/js/home-myfancyplayground.js
@@ -2,3 +2,20 @@
 const myInitializeMyFancyPlaygroundPageStuff = function(){
       logToConsole("Inside: myInitializeMyFancyPlaygroundPageStuff()");
 }
+
+const submitFancyPhoto = function(singleMyPhotoFancyId) {
+   logToConsole(`submitFancyPhoto(): ${singleMyPhotoFancyId}`);
+
+    // const bid = document.getElementById(auctionId + "-input").value;
+    // fetch("/auction/" + auctionId + "/newbid?currentBid=" + bid, {
+    //     method: "POST",
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     }
+    // });
+    // location.reload();
+}
+
+const imageSelectionChanged = function(singleMyPhotoFancyId) {
+    logToConsole(`imageSelectionChanged(): ${singleMyPhotoFancyId}`);
+}
