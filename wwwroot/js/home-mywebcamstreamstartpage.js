3da76a97e831b37ec8c94b6ff02f7a6a32eef95e
Demonically Possessed Exorcist Salman <peaceful.salman@yahoo.com>
2025-10-23 18:45:45 +0300

    Addedd webcampage-site js file to avoid redundancy

--- wwwroot/js/home-mywebcamstreamstartpage.js
+++ wwwroot/js/home-mywebcamstreamstartpage.js
@@ -1,38 +1,5 @@
 
-//--A function/method to initialize our SIgnalR Connection
-//---The SignalR connection is always initiated by the client.
-let myConnection = {};
-const myInitializeSignalRConnection = function () {
-    logToConsole("Inside: myInitializeSignalRConnection()");
 
-    const mySignalRConnection = new signalR.HubConnectionBuilder()
-        .withUrl("/mysuperfancyhub",
-            {
-                transport: signalR.HttpTransportType.WebSockets,
-                skipNegotiation: true
-            }
-        )
-        .configureLogging(signalR.LogLevel.Information)
-        .build();
-
-
-        mySignalRConnection.on("ReceiveNewVideoFrame",
-        (strBase64VideoFrameImage) => {
-
-           let imageForDisplayingVideoFrame = document.getElementById("imageForDisplayingVideoFrame");
-           
-            imageForDisplayingVideoFrame.src = strBase64VideoFrameImage;
-
-        });
-
-
-    mySignalRConnection.start().catch((err) => {
-        return console.error(err.toString());
-    });
-
-    return mySignalRConnection;
-
-};
 
 
 
